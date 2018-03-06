53bc17069efcf16e6510162f1a8d3974b212e0e8
livetoskatee <rcouture27@gmail.com>
2018-03-05 21:18:24 -0500

    Update lightswitch.js

--- lightswitch/lightswitch.js
+++ lightswitch/lightswitch.js
@@ -1,1 +1,13 @@
 
+function start() {
+   let t = document.getElementsById("toggle");
+   t.addEventListener('onclick', toggle);
+}
+
+function toggle() {
+  var color = document.getElementsById("position");
+  color.style.background = blue;
+}
+
+start();
+toggle();
