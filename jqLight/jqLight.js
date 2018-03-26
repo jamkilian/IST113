6c538cb285a0a2ae0486f5fb56af6e4975b68eb0
livetoskatee <rcouture27@gmail.com>
2018-03-26 19:01:24 -0400

    Update jqLight.js

--- jqLight/jqLight.js
+++ jqLight/jqLight.js
@@ -1,1 +1,18 @@
 
+function start() {
+  $("#slider").on("click", toggle);
+};
+
+function toggle() {
+  console.log('whatup');
+  if ($('#body').css('background-color') == 'rgb(255, 255, 255)')
+  {
+      $('#body').css('background-color', 'black');
+  }
+  else
+  {
+      $('#body').css('background-color', 'white');
+  }
+};
+
+start();
