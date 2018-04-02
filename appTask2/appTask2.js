a7ab063bb8abc1f498d9f7e3b164d6f71cfab476
livetoskatee <rcouture27@gmail.com>
2018-04-02 00:16:39 -0400

    Update appTask2.js

--- appTask2/appTask2.js
+++ appTask2/appTask2.js
@@ -1,1 +1,57 @@
+var buttontask = $("#buttontask");
+var task = $("#task");
+var list1 = $("#list1");
+var Delete = $("#delete");
+var Delete1 = $("#delete1");
 
+buttontask.on('click', function () {
+  var newE = $('<li></li>');
+  var textnode = $("#task").val();
+  var buttondelete = $('<div>X</div>');
+
+  Delete.append(buttondelete);
+  newE.append(textnode);
+
+  newE.on('click', function() {
+    this.remove();
+  });
+
+  buttondelete.on('click', function(){
+    this.remove();
+    newE.remove();
+  });
+
+  newE.addClass(task.value);
+  list1.append(newE);
+
+  buttondelete.addClass('delete');
+});
+
+
+
+var buttondesc = $("#buttondesc");
+var list2 = $("#list2");
+var desc = $("#desc");
+
+buttondesc.on('click', function () {
+  var newElem = $('<li></li>');
+  var textnode1 = $("#desc").val();
+  var buttondelete1 = $('<div>X</div>');
+
+  Delete1.append(buttondelete1);
+  newElem.append(textnode1);
+
+  buttondelete1.on('click', function(){
+    this.remove();
+    newElem.remove();
+  });
+
+  newElem.on('click', function() {
+    this.remove();
+  });
+
+  newElem.addClass(desc.value);
+  list2.append(newElem);
+
+  buttondelete1.addClass('delete1');
+});
