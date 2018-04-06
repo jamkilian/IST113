47989e015144bbe858fb9ac78c2db81a66ee1892
livetoskatee <rcouture27@gmail.com>
2018-04-06 17:12:36 -0400

    Update appTask3.js

--- appTask3/appTask3.js
+++ appTask3/appTask3.js
@@ -2,15 +2,17 @@ var buttontask = $("#buttontask");
 var task = $("#task");
 var list1 = $("#list1");
 var Delete = $("#delete");
-var Delete1 = $("#delete1");
 
-var ItemKey = "listValues";
+
+var ItemKey = "Task Values";
 var myItems = [];
 
-function Local() {
+
+
+function Local() { //Task button
   if (localStorage.getItem(ItemKey) !== null)
   {
-    let myItemString = localStorage.getItem(myKey);
+    let myItemString = localStorage.getItem(ItemKey);
     myItems = JSON.parse(myItemString);
     $(myItems).each(function() {
       createItem(this);
@@ -22,73 +24,115 @@ function Local() {
     createItem(curVal);
     myItems.push(curVal);
     saveItems();
-  }
-});
+  });
+};
 
 
 
-function createItem(){
-  var newE = $('<li></li>')
-  var textnode = $("#task").val();
-  var buttondelete = $('<div>X</div>');
-}
-
-buttontask.on('click', function () {
-  var newE = $('<li></li>');
-  var textnode = $("#task").val();
+function createItem(itemValue){ //Task button
+  var newE = $('<li></li>').text(itemValue)
   var buttondelete = $('<div>X</div>');
 
+  list1.append(newE);
   Delete.append(buttondelete);
-  newE.append(textnode);
 
   newE.on('click', function() {
-    this.remove();
+    removeItem(this);
+    buttondelete.remove();
   });
 
   buttondelete.on('click', function(){
-    
     this.remove();
     newE.remove();
+    removeItem(this);
   });
 
-  newE.addClass(task.value);
-  list1.append(newE);
+
 
   buttondelete.addClass('delete');
-});
+};
 
-saveItems();
+function removeItem(item) { //Task button
+  let index = $(item).index();
+  myItems.splice(index, 1);
+  saveItems();
+  $(item).remove();
+}
 
-//Button Description
-var buttondesc = $("#buttondesc");
+
+
+var buttondesc = $("#buttondesc"); //Desc button
 var list2 = $("#list2");
 var desc = $("#desc");
+var Delete1 = $("#delete1");
 
-buttondesc.on('click', function () {
-  var newElem = $('<li></li>');
-  var textnode1 = $("#desc").val();
+var ItemKey2 = "Desc Values";
+var myItems2 = [];
+
+function Local2() { //Desc button
+  if (localStorage.getItem(ItemKey2) !== null)
+  {
+    let myItemString2 = localStorage.getItem(ItemKey2);
+    myItems2 = JSON.parse(myItemString2);
+    $(myItems2).each(function() {
+      createItem2(this);
+    })
+  }
+
+  buttondesc.on('click', function () {
+    var curVal2 = $("#desc").val();
+    createItem2(curVal2);
+    myItems2.push(curVal2);
+    saveItems2();
+  });
+};
+
+
+
+function createItem2(itemValue2){ //Desc button
+  var newElem = $('<li></li>').text(itemValue2)
   var buttondelete1 = $('<div>X</div>');
 
   Delete1.append(buttondelete1);
-  newElem.append(textnode1);
 
   buttondelete1.on('click', function(){
     this.remove();
     newElem.remove();
+    removeItem2(this);
   });
 
   newElem.on('click', function() {
-    this.remove();
+    buttondelete1.remove();
+    removeItem2(this);
   });
 
-  newElem.addClass(desc.value);
   list2.append(newElem);
 
   buttondelete1.addClass('delete1');
-});
-saveItems();
+};
 
-function saveItems() {
-  let myItemString = JSON.stringify(myItems)
-  localStorage.setItem(myItems, myItemString )
+function removeItem2(item2) { //Desc button
+  let index2 = $(item2).index();
+  myItems2.splice(index2, 1);
+  saveItems2();
+  $(item2).remove();
 }
+
+
+function saveItems() { //Task button
+  var myItemString = JSON.stringify(myItems)
+  localStorage.setItem(ItemKey, myItemString )
+}
+
+function saveItems2() { //Desc button
+  var myItemString2 = JSON.stringify(myItems2)
+  localStorage.setItem(ItemKey2, myItemString2 )
+}
+
+$(function() {
+  Local2();
+});
+
+$(function() {
+  Local();
+});
